46ceb6455a0837a7b00d41e0f14f95eff447a3c6
_MeePooh_ <122891690+PluemDontKnowToCode@users.noreply.github.com>
2025-10-08 21:45:31 +0700

    feat: Decorate
    add Quit

--- src/main.py
+++ src/main.py
@@ -18,7 +18,12 @@ CHANNEL_ENTRY = [
     "boat",
     "walkin passenger"
 ]
-
+def clear_screen():
+    try:
+        os.system('cls' if os.name == 'nt' else 'clear')
+    except Exception:
+        sys.stdout.write('\033[2J\033[H')
+        sys.stdout.flush()
 def main():
     hotel = Hotel()
     option = [
@@ -27,10 +32,12 @@ def main():
         "Manully check-out guests",
         "Search room",
         "Export to CSV file",
-        "Print All Room"
+        "Print All Room",
+        "Quit App"
     ]
-    print(BANNER)
+    
     while True:
+        print(BANNER)
         print()
         print("------------------------------------------------")
         print()
@@ -43,40 +50,44 @@ def main():
 
         if input_option == 'q':
             return
-        # try:
-        input_option = int(input_option)
-        
-        match input_option:
-            case 1: #add by channel
-                channels = []
-                for i in range(len(CHANNEL_ENTRY)):
-                    channels.append(int(input(f"{CHANNEL_ENTRY[i]} : ")))
-                hotel.insert(channels=channels)
-                pass
-            case 2: # manual add
-                inp = int(input("Enter guests amount : "))
-                hotel.manual_add(count=inp)
-                pass
-            case 3: # manual remove
-                inp = int(input("Enter room number : "))
-                hotel.manual_remove(room_number=inp)
-                pass
-            # case 4: #rearrange room
-            #     pass
-            case 4: #search room
-                inp = int(input("Enter room number : "))
-                print(hotel.search(room_number=inp))
-                pass
-            case 5: #export to csv
-                hotel.export_to_csv()
-                pass
-            case 6: #print tree
-                hotel.printTree()
-                pass
-            case _:
-                raise SyntaxError("Invalid Input Option")
-        # except:
-        #     print("Invalid Input")
+        try:
+            input_option = int(input_option)
+            
+            match input_option:
+                case 1: #add by channel
+                    channels = []
+                    for i in range(len(CHANNEL_ENTRY)):
+                        channels.append(int(input(f"{CHANNEL_ENTRY[i]} : ")))
+                    hotel.insert(channels=channels)
+                    pass
+                case 2: # manual add
+                    inp = int(input("Enter guests amount : "))
+                    hotel.manual_add(count=inp)
+                    pass
+                case 3: # manual remove
+                    inp = int(input("Enter room number : "))
+                    hotel.manual_remove(room_number=inp)
+                    pass
+                # case 4: #rearrange room
+                #     pass
+                case 4: #search room
+                    inp = int(input("Enter room number : "))
+                    print(hotel.search(room_number=inp))
+                    pass
+                case 5: #export to csv
+                    hotel.export_to_csv()
+                    pass
+                case 6: #print tree
+                    hotel.printTree()
+                    pass
+                case 7: #Quit App
+                    return
+                case _:
+                    raise SyntaxError("Invalid Input Option")
+        except:
+            print("Invalid Input")
+        input("Press enter to continue")
+        clear_screen()
 
 
 if __name__ == "__main__":
